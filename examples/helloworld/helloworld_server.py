b856374419e7a23197b9d5c0be1bb3a76d89f14c
yashbonde <bonde.yash97@gmail.com>
2022-06-22 17:16:01 +0530

    seperate branch for CI/CD

--- examples/helloworld/helloworld_server.py
+++
@@ -1,7 +0,0 @@
-# Auto generated, complete the functions below
-
-from helloworld_pb2 import HelloRequest, HelloReply
-
-class Greeter_Servicer(object):
-  def SayHello(_helloworld_HelloRequest: HelloRequest) -> HelloReply:
-    raise NotImplementedError
