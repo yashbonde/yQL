61908e2038c52bf2bd70ad4fcbb67eb74961292a
yashbonde <bonde.yash97@gmail.com>
2022-07-21 17:13:30 +0530

    manually build a submodule

--- examples/mongo/mongo_rpc_server.py
+++
@@ -1,7 +0,0 @@
-# Auto generated, complete the functions below
-
-from mongo_rpc_pb2 import MongoRequest, MongoResponse
-
-class MongoJsonRPC_Servicer(object):
-  def Call(_MongoRequest: MongoRequest) -> MongoResponse:
-    raise NotImplementedError
